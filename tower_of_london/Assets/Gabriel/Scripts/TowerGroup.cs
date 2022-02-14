007c63917caa0b37183c7cd0575c700cb08b1ce9
abbgabcal <54662435+abbgabcal@users.noreply.github.com>
2022-02-14 13:32:16 +0100

    Fix towerGroup

--- tower_of_london/Assets/Gabriel/Scripts/TowerGroup.cs
+++ tower_of_london/Assets/Gabriel/Scripts/TowerGroup.cs
@@ -15,68 +15,11 @@ public class TowerGroup : MonoBehaviour
         PlateSmallDark,
         PlateSmallSilver
     }
-    public enum Formation
-    {
-        Standard,
-        TwoInVision,
-        OneInVision
-    }
-
-    public Transform playerPos;
-    //public Formation formation;
     public GameObject[] platePrefabs;
-    public Formation formationProp
-    {
-        get { return _formation; }
-        set
-        {
-            _formation = value;
-            Debug.Log("Setter Triggered");
-            switch (value)
-            {
-                case Formation.Standard:
-                    {
-                        Debug.Log("Setting standard");
-                        Debug.Log(playerPos.position.x);
-                        Tower1.transform.position = new Vector3(playerPos.position.x - spacing, playerPos.position.y, playerPos.position.z + distance);
-                        Tower2.transform.position = new Vector3(playerPos.position.x, playerPos.position.y, playerPos.position.z + distance);
-                        Tower3.transform.position = new Vector3(playerPos.position.x + spacing, playerPos.position.y, playerPos.position.z + distance);
 
-                        _formation = Formation.Standard;
-                        //Set locations infront
-                        break;
-                    }
-                case Formation.TwoInVision:
-                    {
-                        _formation = Formation.Standard;
-
-                        break;
-                    }
-                case Formation.OneInVision:
-                    {
-                        _formation = Formation.Standard;
-
-                        break;
-                    }
-
-            }
-        }
-    }
-    public Formation formation; 
-    private Formation _formation;
-
-    private GameObject Tower1;
-    private GameObject Tower2;
-    private GameObject Tower3;
-    private float distance = 8f;
-    private float spacing = 4f;
 
     void Start()
     {
-        Tower1 = gameObject.transform.GetChild(0).gameObject;
-        Tower2 = gameObject.transform.GetChild(1).gameObject;
-        Tower3 = gameObject.transform.GetChild(2).gameObject;
-        formationProp = formation;
         SpawnPlates();
     }
 
@@ -143,15 +86,6 @@ public class TowerGroup : MonoBehaviour
             }
 
         }
-
-        //Tower 1: All of them
-
-
-
-
-        //GameObject newPlate3 = Instantiate(platePrefabs[6], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-        //newPlate3.transform.parent = GameObject.Find("Tower1").transform;
-        //newPlate3.transform.localPosition = new Vector3(0, 2, 0);
 
     }
 
