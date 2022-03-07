d0eb189066bd939ea2badcc1f5982b0b2ecaf066
abbgabcal <54662435+abbgabcal@users.noreply.github.com>
2022-03-07 10:37:23 +0100

    Minor bug fixes.

--- tower_of_london/Assets/Gabriel/Scripts/Tower.cs
+++ tower_of_london/Assets/Gabriel/Scripts/Tower.cs
@@ -17,24 +17,24 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((gameObject.transform.childCount - 2) > PlatesOnTower.Count && PlatesOnTower.Count != 0)
+        if ((gameObject.transform.childCount - 2) > PlatesOnTower.Count)
         {
-            Debug.Log(gameObject);
-            Debug.Log(PlatesOnTower.Count);
-
+            if (PlatesOnTower.Count != 0)
+            {
             PlatesOnTower[PlatesOnTower.Count - 1].isOnTop = false;
+            }
             PlatesOnTower.Add(gameObject.transform.GetChild(gameObject.transform.childCount - 1).GetComponent<Plate>());
             PlatesOnTower[PlatesOnTower.Count - 1].isOnTop = true;
 
         }
         else if ((gameObject.transform.childCount - 2) < PlatesOnTower.Count)
         {
-            Debug.Log(gameObject);
-            Debug.Log(PlatesOnTower.Count);
             PlatesOnTower.RemoveAt(PlatesOnTower.Count - 1);
 
-            PlatesOnTower[PlatesOnTower.Count - 1].isOnTop = true;
-
+            if (PlatesOnTower.Count != 0)
+            {
+                PlatesOnTower[PlatesOnTower.Count - 1].isOnTop = true;
+            }
         }
     }
 }
